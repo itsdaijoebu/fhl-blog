485ea71041fe6724157a7bb09c6c3c46064ac717
Joseph <joseph@josephchan.me>
2023-01-07 13:56:13 -0500

    get posts as partials working

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,9 @@
+const Post = require('../models/Post')
+
 module.exports = {
-    getIndex: (req, res) => {
-        res.render('index.ejs')
+    getIndex: async (req, res) => {
+        const posts = await Post.find().sort({date: 1});
+        console.log(posts);
+        res.render('index.ejs', {posts: posts});
     }
 }

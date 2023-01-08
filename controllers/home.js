f54eccb292b94aef9aed33c6ffc3a68ae14ab6ee
Joseph <joseph@josephchan.me>
2023-01-08 13:47:55 -0500

    add scss build commands

--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@ const Post = require('../models/Post')
 
 module.exports = {
     getIndex: async (req, res) => {
-        const posts = await Post.find().sort({date: 1});
+        const posts = await Post.find().sort({date: -1});
         console.log(posts);
         res.render('index.ejs', {posts: posts});
     }

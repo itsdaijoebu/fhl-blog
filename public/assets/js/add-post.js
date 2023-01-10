83a52f6847db9129f30aceed2be93df90536bdec
Joseph <joseph@josephchan.me>
2023-01-10 15:29:03 -0500

    form submit working with ckeditor and can properly display html posts

--- public/assets/js/add-post.js
+++ public/assets/js/add-post.js
@@ -1,20 +1,36 @@
 const form = document.querySelector('#add-form');
 let editor = document.querySelector('#editor')
 
+
 ClassicEditor
 .create(document.querySelector('#editor'))
 .then(edit => {
     editor = edit
-    console.log(edit)
+    editor.editing.view.document.on(
+        'enter',
+        ( evt, data ) => {
+            editor.execute('shiftEnter');
+            //Cancel existing event
+            data.preventDefault();
+            evt.stop();
+     }, {priority: 'high' });
+
 })
 .catch(error => {
     console.error(error)
 })
 
-console.log(form)
+console.log('form', form)
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    const formData = new FormData(form)
     const content = editor.getData();
-    console.log(content)
+    formData.append('body', content)
+    for(let [key, value] of formData) {
+        console.log('form data', key, value)
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", './add-post', true)
+    xhr.send(formData)
 })
